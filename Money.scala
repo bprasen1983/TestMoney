3965aac059ed8335cd3fa1a0dfdcea3f478a3bca
bprasen1983 <prasenjit.b2@hcl.com>
2017-06-26 22:49:59 +0530

    Modified Money for adding Logger

--- Money.scala
+++ Money.scala
@@ -1,43 +1,66 @@
 package com.hcl.money
 
+/**
+ * Defining Money operations
+ */
 trait Money[A]{
   def add( firstParam : A, secondParam : A) : A
 }
 
-object Money {
-  
-  def apply[A]( implicit m : Money[A] ) : Money[A] = m
-  
-  object ops {
-
-    implicit class MoneyOps[A: Money]( firstParam : A ){
-      def add( secondParam : A ) = Money[A].add( firstParam, secondParam )
-    }
+/**
+ * Defining implicit class to do operation on value like 2 add 3
+ */
+object MoneyMakeUp {
+ implicit class MoneyOps[A: Money]( firstParam : A )( implicit m : Money[A] ){
+   def add( secondParam : A ) = m.add( firstParam, secondParam )
+ }
+}
+/**
+ * MoneyImpl class -- Wrapper class to define how add function should be called
+ */
+class MoneyImpl[A]( m : Money[A]) extends Money[A]{
+  override def add( firstParam : A, secondParam : A ) = m.add(firstParam, secondParam)
+}
+/**
+ * LoggerMoneyImpl class -- Wrapper class to define how add function should be called
+ */
+class LoggerMoneyImpl[A](  m : Money[A]) extends Money[A]{
+  private def log[X]( f: => X) = {
+    println("LOG INFO:START")
+    val x = f
+    println(s"LOG INFO:$x")
+    println("LOG INFO:END")
+    x
   }
-  
-  /**
-   * Declaring Add methods for different data types
-   */
-  def instance[ A ]( funct : (A, A) => A ): Money[A] = new Money[A] {
-    def add( firstParam : A, secondParam : A ) = funct( firstParam, secondParam )
-  }
-  
-  implicit val addInt : Money[Int] = instance[Int]( ( firstParam : Int, secondParam : Int ) => { firstParam + secondParam } )
-  
-  implicit val addBigDecimal : Money[BigDecimal] = instance[BigDecimal]( ( firstParam : BigDecimal, secondParam : BigDecimal ) => { firstParam + secondParam } )
-    
-  implicit val addDOuble : Money[Double] = instance[Double]( ( firstParam : Double, secondParam : Double ) => { firstParam + secondParam } )
-  
+  override def add( firstParam : A, secondParam : A ) = {log( m.add(firstParam, secondParam) ) }
 }
 
+/**
+ * Trait MoneyStructImplicit extending Money trait - defined to help implicit for different data types
+ */
+trait MoneyStructImplicit[A] extends Money[A]
+/**
+ * object MoneyStructImplicit - defined to implement implicit which extends MoneyStructImplicit 
+ */
+object MoneyStructImplicit{
+  implicit object MoneyStructImplicitInt extends MoneyStructImplicit[Int] {
+    override def add( firstParam: Int, secondParam: Int ): Int = firstParam + secondParam
+  }
+}
+/**
+ * Object Money-- define implicit logMoney method
+ */
+object Money{
+  //implicit def logMoney[A]( implicit delegate :MoneyStructImplicit[A]  ) : Money[A] = new MoneyImpl(    delegate    )
+  implicit def logMoney[A]( implicit delegate :MoneyStructImplicit[A]  ) : Money[A] = new LoggerMoneyImpl(    delegate    )
+}
 
 object TestMoney{
   def main(args: Array[String]): Unit = {
-    import Money.ops._
-    println(2 add 3)
-    println( 2.3 add 3.2 )
-    val firstBigDecimalParam : BigDecimal = 1234.12
-    val secondBigDecimalParam : BigDecimal = 1234.12
-    println( firstBigDecimalParam add secondBigDecimalParam   )
+    
+   import MoneyMakeUp._
+   val a = 2  add 3
+   println(a)
+    
   }
 }
